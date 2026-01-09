29b1c9e2e311124c8ec742a363ab60d0423d4e79
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2026-01-09 13:50:33 +0100

    lower case template value

--- components/display_backlight/display_backlight.h
+++ components/display_backlight/display_backlight.h
@@ -71,9 +71,9 @@ namespace esphome
         public:
             SetBrightnessAction(DisplayBacklightComponent *output) : output_(output) {}
 
-            TEMPLATABLE_VALUE(float, Brightness)
+            TEMPLATABLE_VALUE(float, brightness)
 
-            void play(const Ts &...x) override { this->output_->set_brightness(this->Brightness_.value(x...)); }
+            void play(const Ts &...x) override { this->output_->set_brightness(this->brightness_.value(x...)); }
 
         protected:
             DisplayBacklightComponent *output_;
