780e5139084df0fc5128f75decb589a3315a1237
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2026-01-09 11:38:39 +0100

    maybe need to inherit from component?

--- components/display_backlight/sensor.py
+++ components/display_backlight/sensor.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome import automation
 from esphome.components import i2c
 from esphome.const import CONF_BRIGHTNESS, CONF_ID
 
@@ -7,8 +8,30 @@ DEPENDENCIES = ["i2c"]
 
 display_backlight_ns = cg.esphome_ns.namespace("display_backlight")
 DisplayBacklightComponent = display_backlight_ns.class_(
-    "DisplayBacklightComponent", i2c.I2CDevice
+    "DisplayBacklightComponent", cg.Component, i2c.I2CDevice
 )
+
+# TurnOffAction = display_backlight_ns.class_("TurnOffAction", automation.Action)
+# TurnOnAction = display_backlight_ns.class_("TurnOnAction", automation.Action)
+# ToggleAction = display_backlight_ns.class_("ToggleAction", automation.Action)
+
+# TURN_ON_ACTION_SCHEMA = cv.Schema({})
+# TURN_OFF_ACTION_SCHEMA = cv.Schema({})
+# TOGGLE_ACTION_SCHEMA = cv.Schema({})
+
+# @automation.register_action(
+#     "display_backlight.turn_off", TurnOffAction, TURN_OFF_ACTION_SCHEMA
+# )
+# async def display_backlight_turn_off_to_code(config, action_id, template_arg, args):
+#     paren = await cg.get_variable(config[CONF_ID])
+#     return cg.new_Pvariable(action_id, template_arg, paren)
+
+# @automation.register_action(
+#     "display_backlight.toggle", ToggleAction, TOGGLE_ACTION_SCHEMA
+# )
+# async def display_backlight_toggle_to_code(config, action_id, template_arg, args):
+#     paren = await cg.get_variable(config[CONF_ID])
+    # return cg.new_Pvariable(action_id, template_arg, paren)
 
 CONFIG_SCHEMA = (
     cv.Schema(
@@ -18,6 +41,7 @@ CONFIG_SCHEMA = (
         }
     )
     .extend(i2c.i2c_device_schema(0x34))
+    .extend(i2c.i2c)
 )
 
 def to_code(config):
