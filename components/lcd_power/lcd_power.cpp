797240f6de117048101cf0b4329be9ed8a066618
Stefan Lörwald <10850250+stefanloerwald@users.noreply.github.com>
2026-01-17 20:01:18 +0100

    Fix constants to dV instead of mV, such that they fit in uint8_t.

--- components/lcd_power/lcd_power.cpp
+++ components/lcd_power/lcd_power.cpp
@@ -12,9 +12,8 @@ namespace esphome::lcd_power
         constexpr uint8_t XPOWERS_AXP2101_LDO_ONOFF_CTRL0 = 0x90; // For lcd power
         constexpr uint8_t  XPOWERS_AXP2101_LDO_VOL3_CTRL = 0x95; // For lcd voltage
 
-        constexpr uint8_t XPOWERS_AXP2101_ALDO4_VOL_MIN = 500u;
-        constexpr uint8_t XPOWERS_AXP2101_ALDO4_VOL_MAX = 3500u;
-        constexpr uint8_t XPOWERS_AXP2101_ALDO4_VOL_STEPS = 100;
+        constexpr uint8_t XPOWERS_AXP2101_ALDO4_VOL_MIN = 5u;
+        constexpr uint8_t XPOWERS_AXP2101_ALDO4_VOL_MAX = 35u;
 
         constexpr uint8_t _bit_0 = 1u;
         constexpr uint8_t Bit(int which_bit)
@@ -30,12 +29,12 @@ namespace esphome::lcd_power
         read_register(XPOWERS_AXP2101_LDO_ONOFF_CTRL0, &value, 1);
         value |= Bit(4);
         write_register(XPOWERS_AXP2101_LDO_ONOFF_CTRL0, &value, 1);
-        // Setting voltage to 3300mV
-        constexpr uint8_t targetVoltage_mV = 3300u;
+        // Setting voltage to 3300mV = 33dV
+        constexpr uint8_t targetVoltage_mV = 33u;
         value = 0;
         read_register(XPOWERS_AXP2101_LDO_VOL3_CTRL, &value, 1);
         value &= 0xE0;
-        value |= (targetVoltage_mV - XPOWERS_AXP2101_ALDO4_VOL_MIN) / XPOWERS_AXP2101_ALDO4_VOL_STEPS;
+        value |= (targetVoltage_mV - XPOWERS_AXP2101_ALDO4_VOL_MIN);
         write_register(XPOWERS_AXP2101_LDO_VOL3_CTRL, &value, 1);
     }
 
